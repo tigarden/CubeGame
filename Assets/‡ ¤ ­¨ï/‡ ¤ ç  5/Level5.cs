87dd74e90147ef52cbece81ba0ddd3180a63750f
konstantingorbunenko <khorbunenko@gmail.com>
2023-04-15 21:33:57 +0300

    Initial commit

---
+++ Assets/‡ ¤ ­¨ï/‡ ¤ ç  5/Level5.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Level5 : MonoBehaviour
+{
+    private float _speed = 5;
+
+    private void Update()
+    {
+        var step = Time.deltaTime * _speed;
+        var transformPosition = transform.position;
+        transformPosition.x += step;
+
+        transform.position = transformPosition; 
+    }
+}
